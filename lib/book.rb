48267fbf9ec5c2e0d8cf36aeed7ec03cba90198d
Alexandra Weaver <aweaver0124@gmail.com>
2022-01-12 14:26:24 -0600

    commit tree 197404724f39f8f22f534ae615f7acbc32d59ddf

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
+class Book
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+    end
+
+    # author name
+    def author=(author)
+        @author = author
+    end
+    def author
+        @author
+    end
+
+    # page count
+    def page_count=(count)
+        @page_count = count
+    end
+    def page_count
+        @page_count
+    end
+
+    # genre
+    def genre=(genre)
+        @genre = genre
+    end
+    def genre
+        @genre
+    end
+
+    # turn_page
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,46 @@
-# Make your shoe class here!
+class Shoe 
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    # color
+    def color=(color)
+        @color = color
+    end
+    def color
+        @color
+    end
+
+    # size
+    def size=(size)
+        @size = size
+    end
+    def size
+        @size
+    end
+
+    # material
+    def material=(material)
+        @material = material
+    end
+    def material
+        @material
+    end
+
+    # condition
+    def condition=(condition)
+        @condition = condition
+    end
+    def condition
+        @condition
+    end
+
+    # cobble
+    def cobble
+        @condition = 'new'
+        puts "Your shoe is as good as new!"
+    end
+
+end
